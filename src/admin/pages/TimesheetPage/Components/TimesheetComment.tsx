b08a78c98424144490919ccf5405245ba6468b34
Vejn <jsalesev@gmail.com>
2025-10-23 19:06:40 +0200

    formatting comment comp

--- src/admin/pages/TimesheetPage/Components/TimesheetComment.tsx
+++ src/admin/pages/TimesheetPage/Components/TimesheetComment.tsx
@@ -6,19 +6,27 @@ interface TimesheetCommentProps {
     onChange: ( value: string ) => void
 };
 
-const TimesheetComment: React.FC<TimesheetCommentProps> = React.memo( ( { value, onChange } ) => {
+const TimesheetComment: React.FC< TimesheetCommentProps > = React.memo( 
+    ( { value, onChange } ) => {
 
-    const handleChange = useCallback( ( e: React.ChangeEvent<HTMLInputElement> ) => {
-        onChange( e.target.value );
-    }, [ onChange ] );
+        const handleChange = useCallback(
+            ( e: React.ChangeEvent< HTMLInputElement > ) => {
+                onChange( e.target.value );
+            },
+            [ onChange ]
+        );
 
-    return (
-        <Field.Root paddingTop={ 4 } paddingBottom={ 4 }>
-            <Field.Label>Comment:</Field.Label>
-            <Field.Input type="text" value={ value } onChange={ handleChange } />
-        </Field.Root>
-    )
-
-} );
+        return (
+            <Field.Root paddingTop={ 4 } paddingBottom={ 4 }>
+                <Field.Label>Comment:</Field.Label>
+                <Field.Input
+                    type="text"
+                    value={ value }
+                    onChange={ handleChange }
+                />
+            </Field.Root>
+        )
+    }
+);
 
 export default TimesheetComment;        
