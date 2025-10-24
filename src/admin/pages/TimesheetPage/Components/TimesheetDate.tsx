628c88cc7dde295b0c39a0d0b4c19c1732b73820
Vejn <jsalesev@gmail.com>
2025-10-24 09:05:15 +0200

    refactor: timesheet date component

--- src/admin/pages/TimesheetPage/Components/TimesheetDate.tsx
+++ src/admin/pages/TimesheetPage/Components/TimesheetDate.tsx
@@ -7,28 +7,30 @@ interface TimesheetDateProps {
     onChange: ( name: string, value?: Date ) => void
 };
 
-const TimesheetDate: React.FC<TimesheetDateProps> = React.memo( ( { name, value, onChange } ) => {
+const TimesheetDate: React.FC< TimesheetDateProps > = React.memo(
+    ( { name, value, onChange } ) => {
 
-    const handleChange = useCallback( ( d?: Date ) => {
-        onChange( name ,d );
-    }, [ name, onChange ] );
+        const handleChange = useCallback( ( d?: Date ) => {
+            onChange( name ,d );
+        }, [ name, onChange ] );
 
-    const handleClear = useCallback( () => {
-        onChange( name, undefined );
-    }, [ name, onChange ] );
+        const handleClear = useCallback( () => {
+            onChange( name, undefined );
+        }, [ name, onChange ] );
 
-    return (
-        <Field.Root paddingTop={ 4 } paddingBottom={ 4 }>
-            <Field.Label>Date:</Field.Label>
-            <DatePicker
-                value={ value }
-                onChange={ handleChange }
-                onClear={ handleClear }
-                locale="de-DE"
-                size="M"
-            />
-        </Field.Root>
-    )
-} );
+        return (
+            <Field.Root paddingTop={ 4 } paddingBottom={ 4 }>
+                <Field.Label>Date:</Field.Label>
+                <DatePicker
+                    value={ value }
+                    onChange={ handleChange }
+                    onClear={ handleClear }
+                    locale="de-DE"
+                    size="M"
+                />
+            </Field.Root>
+        )
+    }
+);
 
 export default TimesheetDate;        
